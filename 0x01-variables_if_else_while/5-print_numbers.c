3df31ae34cc3ed2816f67526a14bab4c54b3057b
Washtun <washtun@gmail.com>
2022-03-14 07:49:47 -0700

    Scripting in C language

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - main block
+* Description: Printing all numbers of base 10, starting from 0.
+* Return: 0
+*/
+
+int main(void)
+
+{
+
+	int num_base10;
+
+
+
+	for (num_base10 = 0; num_base10 < 10; num_base10++)
+
+	{
+
+		printf("%d\n", num_base10);
+	}
+
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - main block
+* Description: Print all numbers of base 10, starting from 0.
+* Return: 0
+*/
+
+int main(void)
+
+{
+
+	int num_base10;
+
+
+
+	for (num_base10 = 0; num_base10 < 10; num_base10++)
+
+	{
+
+		putchar(int num_base10);
+
+	}
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
