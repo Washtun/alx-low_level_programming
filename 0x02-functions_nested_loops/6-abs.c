294718b3e8f7068ab3b5f4dd9206fd1c6ced83db
Washtun <washtun@gmail.com>
2022-03-16 08:26:00 -0700

    Function to write absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
+
+
+/* Function to find the absolute value*/
+
+int _abs(int N)
+{
+    
+
+    /* If the number is less than
+     * zero, then multiply by (-1)
+     */
+    
+	 if (N < 0) 
+	 {
+        	N = (-1) * N;
+    	 }
+	
+         else
+		 return (N);
+
+}
