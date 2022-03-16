a6ff1759bcfef226d1b1ceb918fbaff119d3b4a8
Washtun <washtun@gmail.com>
2022-03-16 04:51:54 -0700

    Function to print letters of the alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,15 @@
 #include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 
-
-void print_alphabet(void);
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
 
 void print_alphabet(void) 
 {
