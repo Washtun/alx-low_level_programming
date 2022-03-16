d43e806e978443a0caff2a4567cfdacc9e9a03b0
Washtun <washtun@gmail.com>
2022-03-16 06:29:39 -0700

    Functions that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+        int c;
+
+        for (c = 65; c < 123; c++)
+        {
+		if ((char)c >= 'a' && (char)c <= 'z') 
+		{
+			
+			return (1);
+		}
+
+		else 
+		{
+
+			return (0);
+		}
+	
+	}
+
+                
+		_putchar('\n);
+
+       
+
+        return (0);
+}
+
+
