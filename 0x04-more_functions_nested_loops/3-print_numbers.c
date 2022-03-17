10aceeddb3e6804607dcc9fae26e585ab858a11c
Washtun <washtun@gmail.com>
2022-03-17 09:40:30 -0700

    Function that prints 0 to 9, but skips 2 and 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -20,4 +20,3 @@ void print_numbers(void)
 	_putchar('\n');
 
 }
-
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**Function that prints the numbers
+ * from 0 to 9,but skips 2 and 4
+ */
+
+
+void print_numbers(void)
+{
+        int i;
+
+        for (i = 48; i < 58; i++)
+	{	
+		if ((i==2) || (i==4))
+			continue;
+                	
+		_putchar('i');
+	}
+
+        _putchar('\n');
+
+}
