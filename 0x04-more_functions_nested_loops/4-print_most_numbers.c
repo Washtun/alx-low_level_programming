149bcc3254ef8a63a4b186e2b12ecc6c8c57d4eb
Washtun <washtun@gmail.com>
2022-03-18 01:59:11 -0700

    Function to print numbers, from 0 to 9, but skipping 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,10 +13,11 @@ void print_numbers(void)
 
         for (i = 48; i < 58; i++)
 	{	
-		if ((i==2) || (i==4))
+		if ((i == '2') || (i == '4'))
 			continue;
-                	
-		_putchar('i');
+                
+		else
+			_putchar('i');
 	}
 
         _putchar('\n');
