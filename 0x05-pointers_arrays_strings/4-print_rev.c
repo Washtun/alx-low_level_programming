d5168bc40e43e61bb78bf514a23f3fcbf6e958a5
Washtun <washtun@gmail.com>
2022-03-22 06:08:45 -0700

    A function that prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - function that write a string in reverse order
+ *
+ * Return: Always 
+ */
+
+void print_rev(char *s)
+{
+	int i, len, temp;
+	len = _strlen(s);
+
+	for(i =0; i < len/2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+		_putchar(temp);
+	}
+	_putchar('\n');
+}
